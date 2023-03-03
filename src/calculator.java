c43a30eb7b291fa4d23f5774cfe05a793b033c0c
Manul Thanura <manulkarunarathna@gmail.com>
2023-03-03 19:51:23 +0530

    Update

--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 public class calculator {
+
     private JTextField textField2;
     private JButton button1;
     private JButton button2;
@@ -8,4 +9,22 @@ public class calculator {
     private JButton button4;
     private JButton button3;
     private JTextField textField1;
+
+    public void setData(Main data) {
+        textField2.setText(data.getTextField2());
+        textField1.setText(data.getTextField1());
+    }
+
+    public void getData(Main data) {
+        data.setTextField2(textField2.getText());
+        data.setTextField1(textField1.getText());
+    }
+
+    public boolean isModified(Main data) {
+        if (textField2.getText() != null ? !textField2.getText().equals(data.getTextField2()) : data.getTextField2() != null)
+            return true;
+        if (textField1.getText() != null ? !textField1.getText().equals(data.getTextField1()) : data.getTextField1() != null)
+            return true;
+        return false;
+    }
 }
